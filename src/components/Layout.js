ee27b19e22536dc8b4323787997c9930f17e9e7b
Chase <chase.yang@city.ac.uk>
2024-01-13 00:20:36 +0000

    Created sidenav

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+//import { Outlet } from 'react-router-dom'
+import Navbar from "./Navbar"
+
+
+const Layout = () => {
+    return (
+        <div className={''}>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Layout
---
+++ src/components/Navbar.js
@@ -0,0 +1,75 @@
+import {Link, NavLink} from 'react-router-dom'
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import '../App.css'
+import CLogo from '../assets/chaselogo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faLinkedin,
+    faGithub,
+} from '@fortawesome/free-brands-svg-icons'
+const Navigation = () => {
+    return (
+        <div className={"nav-bar d-flex"}>
+            <Link to={"/"} className={"logo"}>
+                <img src={CLogo} alt="brand logo"/>
+            </Link>
+                <nav>
+                       <NavLink exact="true" to="/" >Home</NavLink>
+                       <NavLink to="/about" >About</NavLink>
+                       <NavLink to="/skills" >Skills</NavLink>
+                       <NavLink to="/projects" >Projects</NavLink>
+                       <NavLink to="/contact" >Contact</NavLink>
+                </nav>
+            <ul>
+                <li>
+                    <a href="https://www.linkedin.com/in/chase-yang/"
+                       target="_blank" rel="noreferrer">
+                        <FontAwesomeIcon
+                        icon={faLinkedin}
+                        color="#4d4d4e"
+                        className="anchor-icon"/></a>
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/ChaseY03"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <FontAwesomeIcon
+                            icon={faGithub}
+                            color="#4d4d4e"
+                            className="anchor-icon"
+                        />
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+    )
+}
+
+export default Navigation
+
+/*
+ <Navbar fixed="left" expand="md">
+            <Container>
+            <Navbar.Brand href="/" className={"d-flex"}>
+                <img src={CLogo} alt="brand logo" className={"img-fluid logo"}/>
+            </Navbar.Brand>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/about" >To About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/skills" >To Skills</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/projects" >To Projects</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/contact" >To Contact</Nav.Link>
+            </Nav.Item>
+            </Container>
+        </Navbar>
+*/
